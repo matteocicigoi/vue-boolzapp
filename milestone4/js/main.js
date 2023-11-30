005ce2efbfe041e81623db0823a01833ccbd3974
matteocicigoi <matteo.cicigoi1@hotmail.it>
2023-11-30 14:07:01 +0100

    feat: milestone4

---
+++ milestone4/js/main.js
@@ -0,0 +1,232 @@
+'use strict';
+
+const { createApp } = Vue;
+
+// profilo personale
+const user = [
+    {
+        name: 'Nome Utente',
+        avatar: './img/avatar_io.jpg'
+    }
+];
+
+// lista utenti
+const contacts = [
+    {
+        name: 'Michele',
+        avatar: './img/avatar_1.jpg',
+        visible: true,
+        messages: [
+            {
+                date: '10/01/2020 15:30:55',
+                message: 'Hai portato a spasso il cane?',
+                status: 'sent'
+            },
+            {
+                date: '10/01/2020 15:50:00',
+                message: 'Ricordati di stendere i panni',
+                status: 'sent'
+            },
+            {
+                date: '10/01/2020 16:15:22',
+                message: 'Tutto fatto!',
+                status: 'received'
+            }
+        ],
+    },
+    {
+        name: 'Fabio',
+        avatar: './img/avatar_2.jpg',
+        visible: true,
+        messages: [
+            {
+                date: '20/03/2020 16:30:00',
+                message: 'Ciao come stai?',
+                status: 'sent'
+            },
+            {
+                date: '20/03/2020 16:30:55',
+                message: 'Bene grazie! Stasera ci vediamo?',
+                status: 'received'
+            },
+            {
+                date: '20/03/2020 16:35:00',
+                message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                status: 'sent'
+            }
+        ],
+    },
+    {
+        name: 'Samuele',
+        avatar: './img/avatar_3.jpg',
+        visible: true,
+        messages: [
+            {
+                date: '28/03/2020 10:10:40',
+                message: 'La Marianna va in campagna',
+                status: 'received'
+            },
+            {
+                date: '28/03/2020 10:20:10',
+                message: 'Sicuro di non aver sbagliato chat?',
+                status: 'sent'
+            },
+            {
+                date: '28/03/2020 16:15:22',
+                message: 'Ah scusa!',
+                status: 'received'
+            }
+        ],
+    },
+    {
+        name: 'Alessandro B.',
+        avatar: './img/avatar_4.jpg',
+        visible: true,
+        messages: [
+            {
+                date: '10/01/2020 15:30:55',
+                message: 'Lo sai che ha aperto una nuova pizzeria?',
+                status: 'sent'
+            },
+            {
+                date: '10/01/2020 15:50:00',
+                message: 'Si, ma preferirei andare al cinema',
+                status: 'received'
+            }
+        ],
+    },
+    {
+        name: 'Alessandro L.',
+        avatar: './img/avatar_5.jpg',
+        visible: true,
+        messages: [
+            {
+                date: '10/01/2020 15:30:55',
+                message: 'Ricordati di chiamare la nonna',
+                status: 'sent'
+            },
+            {
+                date: '10/01/2020 15:50:00',
+                message: 'Va bene, stasera la sento',
+                status: 'received'
+            }
+        ],
+    },
+    {
+        name: 'Claudia',
+        avatar: './img/avatar_6.jpg',
+        visible: true,
+        messages: [
+            {
+                date: '10/01/2020 15:30:55',
+                message: 'Ciao Claudia, hai novità?',
+                status: 'sent'
+            },
+            {
+                date: '10/01/2020 15:50:00',
+                message: 'Non ancora',
+                status: 'received'
+            },
+            {
+                date: '10/01/2020 15:51:00',
+                message: 'Nessuna nuova, buona nuova',
+                status: 'sent'
+            }
+        ],
+    },
+    {
+        name: 'Federico',
+        avatar: './img/avatar_7.jpg',
+        visible: true,
+        messages: [
+            {
+                date: '10/01/2020 15:30:55',
+                message: 'Fai gli auguri a Martina che è il suo compleanno!',
+                status: 'sent'
+            },
+            {
+                date: '10/01/2020 15:50:00',
+                message: 'Grazie per avermelo ricordato, le scrivo subito!',
+                status: 'received'
+            }
+        ],
+    },
+    {
+        name: 'Davide',
+        avatar: './img/avatar_8.jpg',
+        visible: true,
+        messages: [
+            {
+                date: '10/01/2020 15:30:55',
+                message: 'Ciao, andiamo a mangiare la pizza stasera?',
+                status: 'received'
+            },
+            {
+                date: '10/01/2020 15:50:00',
+                message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
+                status: 'sent'
+            },
+            {
+                date: '10/01/2020 15:51:00',
+                message: 'OK!!',
+                status: 'received'
+            }
+        ],
+    }
+];
+
+createApp({
+  data() {
+    return {
+        user,
+        contacts,
+        currentChat: 0,
+        textMessage: '',
+        searchUser: ''
+    }
+  },
+  methods: {
+    // seleziona la chat in base al indice
+    selectChat(index){
+        this.currentChat = index;
+    },
+    // invia un messaggio
+    sendMessage(type){
+        if(type !== 'send'){
+            this.contacts[this.currentChat].messages.push({
+                date: this.dateFn('full'),
+                message: 'ok',
+                status: 'received'
+            });
+        }else{
+            this.contacts[this.currentChat].messages.push({
+                date: this.dateFn('full'),
+                message: this.textMessage,
+                status: 'sent'
+            });
+            this.textMessage = '';
+            // risposta dopo 1 secondo
+            setTimeout(this.sendMessage, 1000);
+        }
+    },
+    // data
+    dateFn(type){
+        if(type === 'full'){
+            // se type è uguale a full genera una data
+            const date = new Date;
+            return `${date.getDate()}/${date.getMonth()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+        }
+        // recupera i l'ora e i minuti
+        const time = [];
+        for(let i = 11; i < 16; i++){
+            time.push(type[i]);
+        }
+        return time.join('');
+    },
+    // ricerca
+    findUser(){
+        return this.contacts.filter((user) => user.name.toLowerCase().includes(this.searchUser.toLowerCase()));
+    }
+}
+}).mount('#app');
+//'10/01/2020 15:30:55'
